27d965ee1a6fc813a7454a2cb854df0af0e676e1
Поночовний Олександр Вікторович <59876279+sasha2122@users.noreply.github.com>
2021-05-21 13:19:35 +0300

    lab3

---
+++ lab3/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора..
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(522, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(522, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 578);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Click \'OK\' to continue!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab3/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+
+  
+
+    public partial class Form1 : Form
+    {
+        int CursorX = Cursor.Position.X;
+        int CursorY = Cursor.Position.Y;
+        bool flag = false;
+        bool flag2 = false;
+        int i = 0;
+        int j = 0;
+        public Form1()
+        {
+            InitializeComponent();        
+            this.timer1.Enabled = true;
+            this.MouseMove += button1_MouseMove;
+            this.button1.TabStop = false;
+           
+        }
+        int b = 0;
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            int delta = 30;
+            int speed = 2;
+            int bX = button1.Location.X;
+            int bY = button1.Location.Y;
+            int width = button1.Width;
+            int height = button1.Height;
+
+            if (e.X > (bX - delta) & e.Y < bY & e.X < bX & e.Y > (bY - delta))
+            {
+                this.button1.Location = new Point(bX + speed, bY + speed);
+                if (b == 15)
+                {
+                 this.button1.Height -= 1;
+                this.button1.Width -= 1;
+                }
+                b++;
+            }
+            else if(e.X > bX & e.Y < bY & e.X < bX + width & e.Y > bY - delta)
+            {
+                this.button1.Location = new Point(bX, bY + speed);
+                if (b == 5)
+                {
+                    this.button1.Height -= 1;
+                    this.button1.Width -= 1;
+                }
+                b++;
+            }
+            else if(e.X > bX + width & e.Y < bY & e.X < bX + width + delta & e.Y > bY - delta)
+            {
+                this.button1.Location = new Point(bX - speed, bY + speed);
+                if (b == 5)
+                {
+                    this.button1.Height -= 1;
+                    this.button1.Width -= 1;
+                    
+                }
+                b++;
+            }
+            else if (e.X > (bX - delta) & (e.X < bX) & e.Y > bY & e.Y < bY + height) 
+            {
+                this.button1.Location = new Point(bX + speed, bY);
+                if (b == 5)
+                {
+                    this.button1.Height -= 1;
+                    this.button1.Width -= 1;
+
+                }
+                b++;
+            }
+            else if (e.X < (bX + delta + width) & (e.X > bX) & e.Y > bY & e.Y < bY + height) // влево
+            {
+                button1.Location = new Point(bX - speed, bY);
+                if (b == 5)
+                {
+                    this.button1.Height -= 1;
+                    this.button1.Width -= 1;
+                }
+                b++;
+            }
+            else if (e.X > (bX - delta) & e.Y > bY & e.X < bX & e.Y < (bY + delta + height))
+            {
+                this.button1.Location = new Point(bX + speed, bY - speed);
+                if (b == 5)
+                {
+                    this.button1.Height -= 1;
+                    this.button1.Width -= 1;
+                }
+                b++;
+            }
+            else if (e.X > bX & e.Y > bY & e.X < bX + width & e.Y < (bY + delta + height))
+            {
+                this.button1.Location = new Point(bX, bY - speed);
+                if (b == 5)
+                {
+                    this.button1.Height -= 1;
+                    this.button1.Width -= 1;
+                }
+                b++;
+            }
+            else if (e.X > bX + width & e.Y > bY & e.X < bX + width + delta & e.Y < (bY + delta + height))
+            {
+                this.button1.Location = new Point(bX - speed, bY - speed);
+                if (b == 5)
+                {
+                    this.button1.Height -= 1;
+                    this.button1.Width -= 1;
+                }
+                b++;
+            }
+            
+            if (b == 6)
+            {
+                b = 0;
+            }
+       
+            if (bX >= this.Size.Width - this.button1.Width || bY >= this.Size.Height - this.button1.Height - 30)
+            {
+                this.button1.Location = new Point(bX - 75, bY - 75);
+            }
+            if (bX <= 0 || bY <= 0)
+            {
+                this.button1.Location = new Point(bX + 70, bY + 70);
+            }
+            if(this.button1.Height == 1 || this.button1.Width == 1)
+            {
+                this.timer2.Enabled = true;
+            }
+
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (flag)
+            {
+                this.Text = "Click 'OK' to continue!";
+                flag = false;
+            }
+            else
+            {
+                this.Text = "";
+                flag = true;
+            }
+
+            if (i == 7)
+            {
+                this.timer1.Stop();
+            }
+            i++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Кнопка Ок натиснена");
+           
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            if (flag2)
+            {
+                this.Text = "'OK' will never be clicked!";
+                flag2 = false;
+            }
+            else
+            {
+                this.Text = "";
+                flag2 = true;
+            }
+            if (j >= 7)
+            {
+                this.timer2.Stop();
+            }
+
+            j++;
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                e.Handled = true;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (!msg.HWnd.Equals(this.Handle) && (keyData == Keys.Left || keyData == Keys.Down || keyData == Keys.Up ||
+                                                  keyData == Keys.Right))
+                return true;
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+    }
+}
